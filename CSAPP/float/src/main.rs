3747d9ad648e7ab5da61e6638bb9a60acf89b294
ivfranco <ivfranco33@outlook.com>
2018-02-14 12:38:37 +0800

    o

--- CSAPP/float/src/main.rs
+++ CSAPP/float/src/main.rs
@@ -6,7 +6,7 @@ use std::mem;
 use rand::Rng;
 
 fn main() {
-    practice_2_63();
+    practice_2_79();
 }
 
 fn practice_2_47() {
@@ -130,4 +130,243 @@ fn any_odd_one(x: u32) -> bool {
     // a mask that have 1 at every odd position
     let mask = 0x55555555;
     mask & x != 0
+}
+
+fn odd_ones(x: u32) -> bool {
+    let mut y = x;
+    y ^= y >> 16;
+    y ^= y >> 8;
+    y ^= y >> 4;
+    y ^= y >> 2;
+    y ^= y >> 1;
+    y & 1 == 1
+}
+
+fn practice_2_65() {
+    let x: u32 = rand::random();
+    println!("{:b}, {} == {}", x, x.count_ones() % 2 == 1, odd_ones(x));
+}
+
+fn leftmost_one(x: u32) -> u32 {
+    let mut y = x;
+    y |= y >> 1;
+    y |= y >> 2;
+    y |= y >> 4;
+    y |= y >> 8;
+    y |= y >> 16;
+
+    ((y >> 1) + 1) & x
+}
+
+fn practice_2_66() {
+    let x: u32 = rand::random();
+    println!("{:032b}\n{:032b}\n", x, leftmost_one(x));
+}
+
+fn lower_one_mask(n: usize) -> usize {
+    let w = mem::size_of::<usize>() << 3;
+    assert!(1 <= n && n <= w);
+
+    let mut y = 1usize;
+    y = y << (n - 1);
+    ((y - 1) << 1) | 1
+}
+
+fn practice_2_68() {
+    for i in 1..65 {
+        println!("{:064b}", lower_one_mask(i));
+    }
+}
+
+fn rotate_left(x: usize, n: usize) -> usize {
+    let w = mem::size_of::<usize>() << 3;
+    assert!(n < w);
+
+    let higher_bits = x >> (w - n - 1) >> 1;
+    (x << n) | higher_bits
+}
+
+fn practice_2_69() {
+    let mut rnd = rand::thread_rng();
+    let x = rnd.gen();
+    let n = rnd.gen_range(0, mem::size_of::<usize>() * 8);
+    println!("{:016X}, {}", x, n);
+    println!("{:016X}", rotate_left(x, n));
+    println!("{:016X}\n", x.rotate_left(n as u32));
+}
+
+fn fit_bits(x: isize, n: usize) -> bool {
+    let w = mem::size_of::<isize>() << 3;
+    assert!(1 <= n && n <= w);
+
+    // would be equivalent if inlined the function body of lower_one_mask, no function call
+    let lower_mask = unsafe { mem::transmute::<usize, isize>(lower_one_mask(n)) };
+    // either x is an n-bit negative number extended to w bits (i.e. higher (w - n) bits all equal to 1)
+    // or x is an (n-1) bit nonnegative number extended to w bits (i.e. higher (w - n + 1) bits all equal to 0)
+    !(x | lower_mask) == 0 || (x & !(lower_mask >> 1)) == 0
+}
+
+fn practice_2_70() {
+    fn nbit_min_max(n: usize) -> (isize, isize) {
+        assert!(n <= mem::size_of::<isize>() * 8 - 1);
+        let max = (1isize << n).wrapping_sub(1);
+        let min = (1isize << n).wrapping_neg();
+        (min, max)
+    }
+
+    fn arith_fit_bits(x: isize, n: usize) -> bool {
+        if n >= mem::size_of::<isize>() * 8 {
+            return true;
+        }
+        let (min, max) = nbit_min_max(n);
+        min <= x && x <= max
+    }
+
+    let w = mem::size_of::<isize>() * 8;
+    let mut rnd = rand::thread_rng();
+    let n = rnd.gen_range(1, w);
+    let k = rnd.gen_range(1, w);
+    let (min, max) = nbit_min_max(k);
+    let x = rnd.gen_range(min, max);
+    println!("{:016x}, {}", x, n);
+    println!("{} == {}", fit_bits(x, n), arith_fit_bits(x, n));
+}
+
+fn saturating_add(x: isize, y: isize) -> isize {
+    let mut sum = x.wrapping_add(y);
+    // has msb 1; other bits are 0
+    let min = isize::min_value();
+    // has msb 0; other bits are 1
+    let max = !min;
+    let w = mem::size_of::<isize>() << 3;
+
+    let msb_x = x & min;
+    let msb_y = y & min;
+    let msb_s = sum & min;
+
+    // a number has msb 1 if the addition overflowed, 0 otherwise; other bits are 0
+    let overflowed = !msb_x & !msb_y & msb_s;
+    // a number has msb 1 if the addition underflowed, 0 otherwise; other bits are 0
+    let underflowed = msb_x & msb_y & !msb_s;
+
+    // a number has msb 0 if addition overflowed, 1 otherwise; other bits are 1
+    let overflow_msb_mask = !overflowed;
+    sum &= overflow_msb_mask;
+    // a number has all bits except msb equal to 1 if overflowed, 0 otherwise; msb is 0
+    let overflow_rest_mask = overflowed >> (w - 1) & max;
+    sum |= overflow_rest_mask;
+
+    // a number has msb 1 if addition underflowed, 0 otherwise; other bits are 0
+    let underflow_msb_mask = underflowed;
+    sum |= underflow_msb_mask;
+    // a number has all bits except msb equal to 0 if underflowed, 1 otherwise; msb is 1
+    let underflow_rest_mask = !(underflowed >> (w - 1)) | min;
+    sum &= underflow_rest_mask;
+
+    sum
+}
+
+fn practice_2_73() {
+    let x: isize = rand::random();
+    let y: isize = rand::random();
+    let lib_res = x.saturating_add(y);
+    let usr_res = saturating_add(x, y);
+    println!("{} == {}, {}", lib_res, usr_res, lib_res == usr_res);
+}
+
+fn unsigned_high_prod(x: u32, y: u32) -> u32 {
+    fn signed_high_prod(x: i32, y: i32) -> i32 {
+        let p = (x as i64) * (y as i64);
+        let low_and_high = unsafe { mem::transmute::<i64, [i32; 2]>(p) };
+        low_and_high[1]
+    }
+
+    let sig_x = if x > i32::max_value() as u32 { 1 } else { 0 };
+    let sig_y = if y > i32::max_value() as u32 { 1 } else { 0 };
+    unsafe {
+        let tx = mem::transmute::<u32, i32>(x);
+        let ty = mem::transmute::<u32, i32>(y);
+        let tp = mem::transmute::<i32, u32>(signed_high_prod(tx, ty));
+        tp.wrapping_add(sig_x * y).wrapping_add(sig_y * x)
+    }
+}
+
+fn practice_2_75() {
+    fn arith_high_prod(x: u32, y: u32) -> u32 {
+        let p = (x as u64) * (y as u64);
+        let low_and_high = unsafe { mem::transmute::<u64, [u32; 2]>(p) };
+        low_and_high[1]
+    }
+
+    let x = rand::random();
+    let y = rand::random();
+    let arith_prod = arith_high_prod(x, y);
+    let usr_prod = unsigned_high_prod(x, y);
+    println!("{}\n{}, {}\n", arith_prod, usr_prod, arith_prod == usr_prod);
+}
+
+fn divide_power2(x: isize, k: usize) -> isize {
+    let w = mem::size_of::<isize>() << 3;
+    assert!(k < w);
+
+    // a number that has 1 at k-th position, other bits are 0; mask == 0 when k == 0
+    let mask = (1 << k) >> 1;
+    // Rust do not have C-style ! operator
+    // simulating int bias = !!(x & mask) with conditions
+    let bias = if x & mask == 0 { 0 } else { 1 };
+    let sig_x = x & isize::min_value();
+    let sig_mask = ((bias << (w - 1)) & sig_x) >> (w - 2);
+
+    (x >> k) + (bias | sig_mask)
+}
+
+fn practice_2_77() {
+    let mut rnd = rand::thread_rng();
+    let x = rnd.gen();
+    let k = rnd.gen_range(0, mem::size_of::<isize>() * 8);
+    println!(
+        "{:064b}\n{:064b}\n{:064b}\n",
+        x,
+        x >> k,
+        divide_power2(x, k)
+    );
+}
+
+fn mul3div4(x: isize) -> isize {
+    ((x << 1) + x) >> 2
+}
+
+fn threefourths(x: isize) -> isize {
+    let higher = x & !0b11;
+    let lower = x & 0b11;
+
+    let tf_higher = (higher >> 2) + (higher >> 1);
+    let tf_lower = ((lower << 1) + lower) >> 2;
+
+    // if ((lower << 1) + lower) & 0b11 != 0 and x < 0, tf_higher + tf_lower is one less than the result
+    // simulating !!(x & INT_MIN)
+    let sig_x = if x & isize::min_value() == 0 { 0 } else { 1 };
+    // simulating !!(((lower << 1) + lower) & 3)
+    let rounded = if ((lower << 1) + lower) & 0b11 == 0 {
+        0
+    } else {
+        1
+    };
+    let bias = sig_x & rounded;
+
+    tf_higher + tf_lower + bias
+}
+
+fn practice_2_79() {
+    assert!(threefourths(8) == 6);
+    assert!(threefourths(9) == 6);
+    assert!(threefourths(10) == 7);
+    assert!(threefourths(11) == 8);
+    assert!(threefourths(12) == 9);
+
+    assert!(threefourths(-8) == -6);
+    assert!(threefourths(-9) == -6);
+    assert!(threefourths(-10) == -7);
+    assert!(threefourths(-11) == -8);
+    assert!(threefourths(-12) == -9);
 }
