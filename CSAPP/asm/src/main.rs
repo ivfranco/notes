f7f2bbf60790b3ad58c91d33bbc508c723f34812
ivfranco <ivfranco33@outlook.com>
2018-02-18 13:07:46 +0800

    o

---
+++ CSAPP/asm/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+fn decode2(x: i32, y: i32, z: i32) -> i32 {
+    let diff = y - z;
+    let mut ret = diff << 31;
+    ret >>= 31;
+    ret ^ (x * diff)
+}
