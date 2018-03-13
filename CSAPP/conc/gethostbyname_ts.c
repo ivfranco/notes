3f194110b4e485fb541900c57e93b1e3f3e94c3e
ivfranco <ivfranco33@outlook.com>
2018-03-13 17:34:36 +0800

    o

---
+++ CSAPP/conc/gethostbyname_ts.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <semaphore.h>
+#include <string.h>
+
+static sem_t mutex;
+
+void init();
+size_t array_len(char **);
+char **deep_copy_strs(char **);
+struct hostent *gethostbyname_ts(char *);
+
+int main(int argc, char *argv[]) {
+    struct hostent *hostp;
+    struct in_addr addr;
+    char **pp, *name;
+
+    init();
+    name = argv[1];
+    hostp = gethostbyname_ts("github.com");
+    for (pp = hostp->h_addr_list; *pp != NULL; pp++) {
+        addr.s_addr = ((struct in_addr *)*pp) -> s_addr;
+        printf("address: %s\n", inet_ntoa(addr));
+    }
+
+    return 0;
+}
+
+void init() {
+    sem_init(&mutex, 0, 1);
+}
+
+size_t array_len(char **ptr) {
+    size_t cnt = 0;
+    while (*ptr != NULL) {
+        ptr++;
+        cnt++;
+    }
+    return cnt;
+}
+
+char **deep_copy_strs(char **ss) {
+    char **cpy = (char **)malloc((array_len(ss) + 1) * sizeof(char *));
+    char **cpy_iter = cpy;
+
+    while (*ss != NULL) {
+        *cpy_iter = strdup(*ss);
+        ss++;
+        cpy_iter++;
+    }
+    *cpy_iter = NULL;
+    return cpy;
+}
+
+struct hostent *gethostbyname_ts(char *name) {
+    struct hostent *hostp, *hostp_s;
+    
+    // printf("acquiring semaphore\n");
+    sem_wait(&mutex);
+    // printf("acquired semaphore\n");
+    hostp = (struct hostent *)malloc(sizeof(struct hostent));
+    hostp_s = gethostbyname(name);
+    // printf("gethostbyname returned\n");
+    hostp->h_addrtype = hostp_s->h_addrtype;
+    hostp->h_length = hostp_s->h_length;
+    hostp->h_name = strdup(hostp_s->h_name);
+    hostp->h_aliases = deep_copy_strs(hostp_s->h_aliases);
+    hostp->h_addr_list = deep_copy_strs(hostp_s->h_addr_list);
+    // printf("releasing semaphore\n");
+    sem_post(&mutex);
+    // printf("released semaphore\n");
+    return hostp;
+}
--- CSAPP/conc/src/main.rs
+++ CSAPP/conc/src/main.rs
@@ -1,11 +1,31 @@
 mod echo;
 mod prethread;
 
+use std::env;
+use std::thread;
+use std::sync::{Arc, Mutex};
 // use echo::echo_server;
-use prethread::echo_server;
+// use prethread::echo_server;
 
 fn main() {
-    if let Err(e) = echo_server() {
-        eprintln!("{}", e);
+    problem_12_16();
+}
+
+fn problem_12_16() {
+    let n: usize = env::args()
+        .nth(1)
+        .expect("First argument missing")
+        .parse()
+        .expect("Invalid unsigned integer");
+
+    let mut thread_vec = Vec::with_capacity(n);
+    for i in 0..n {
+        thread_vec.push(thread::spawn(move || {
+            println!("Thread {} spawned", i);
+        }));
+    }
+    for (i, handle) in thread_vec.into_iter().enumerate() {
+        handle.join();
+        println!("Thread {} reaped", i);
     }
 }
