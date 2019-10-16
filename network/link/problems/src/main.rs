67369f19ad1f2e447b24b4c0acadaa716671e199
ivfranco <ivfranco33@outlook.com>
2019-10-16 18:01:38 +0800

    p

--- network/link/problems/src/main.rs
+++ network/link/problems/src/main.rs
@@ -1,4 +1,5 @@
 use problems::{checksum, short_crc};
+use textplots::{Chart, Plot, Shape};
 
 fn main() {
     problem_3();
@@ -6,6 +7,7 @@ fn main() {
     problem_5();
     problem_6();
     problem_7();
+    problem_12();
 }
 
 fn bin(checksum: [u8; 2]) -> String {
@@ -50,5 +52,36 @@ fn problem_7() {
     for data in 0b1000 ..= 0b1111 {
         println!("{:04b} XOR 1001 = {:04b}", data, data ^ 0b1001);
         assert_eq!(parity(data), parity(data ^ 0b1001));
+    }
+}
+
+fn problem_12() {
+    println!("\nP12");
+
+    for &n in &[15, 25, 35] {
+        println!("N = {}", n);
+
+        let nf = n as f32;
+
+        let slotted = (0 ..= 100)
+            .map(|p| {
+                let p = p as f32 / 100.0;
+                let e = nf * p * (1.0 - p).powi(n - 1);
+                (p, e)
+            })
+            .collect::<Vec<_>>();
+
+        let pure = (0 ..= 100)
+            .map(|p| {
+                let p = p as f32 / 100.0;
+                let e = nf * p * (1.0 - p).powi(2 * n - 2);
+                (p, e)
+            })
+            .collect::<Vec<_>>();
+
+        Chart::new(120, 60, 0.0, 1.0)
+            .lineplot(Shape::Lines(&slotted))
+            .lineplot(Shape::Lines(&pure))
+            .display();
     }
 }
