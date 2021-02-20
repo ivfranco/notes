b5ea273525e0c20c74a2f2911a4b2949b3eff876
ivfranco <ivfranco33@outlook.com>
2021-02-20 13:13:08 +0800

    p

--- database/model/ER.ts
+++ database/model/ER.ts
@@ -1,6 +1,8 @@
-import graphviz from "graphviz";
+import graphviz from 'graphviz';
 
-export { Arrow, Entity, Relation, ERModel, RelationKind, binary_relation };
+export {
+  Arrow, Entity, Relation, ERModel, RelationKind, binary_relation,
+};
 
 interface Entity {
   label: string;
@@ -15,14 +17,21 @@ enum Arrow {
 function arrow_style(arrow: Arrow): graphviz.Options {
   switch (arrow) {
     case Arrow.Many:
-      return { arrowhead: "none" };
+      return { arrowhead: 'none' };
     case Arrow.One:
-      return { arrowhead: "normal" };
+      return { dir: "forward", arrowhead: 'normal' };
+    default:
+      throw Error('unreachable');
   }
 }
 
 interface Relation extends Entity {
   arrows: Array<[Entity, Arrow]>;
+}
+
+interface ISA {
+  base: Entity;
+  child: Entity;
 }
 
 enum RelationKind {
@@ -34,10 +43,10 @@ enum RelationKind {
 
 function binary_relation(label: string, from: Entity, to: Entity, kind?: RelationKind): Relation {
   kind = kind ?? RelationKind.ManyMany;
-  let from_arrow = kind === RelationKind.ManyMany || kind == RelationKind.ManyOne ? Arrow.Many : Arrow.One;
-  let to_arrow = kind === RelationKind.ManyMany || kind == RelationKind.OneMany ? Arrow.Many : Arrow.One;
+  const from_arrow = kind === RelationKind.ManyMany || kind == RelationKind.ManyOne ? Arrow.Many : Arrow.One;
+  const to_arrow = kind === RelationKind.ManyMany || kind == RelationKind.OneMany ? Arrow.Many : Arrow.One;
   return {
-    label: label,
+    label,
     arrows: [
       [from, from_arrow],
       [to, to_arrow],
@@ -49,43 +58,59 @@ class ERModel {
   inner: graphviz.Graph;
 
   constructor(label: string) {
-    this.inner = graphviz.digraph(label);
+    this.inner = graphviz.graph(label);
   }
 
   private add_cluster(entity: Entity, shape: string): graphviz.Node {
-    let g = this.inner;
+    const g = this.inner;
 
     // name of a subgraph cannot contain `-` or `.`
-    let cluster = g.addCluster(`Cluster_${entity.label.replace("-", "_")}`);
-    cluster.set("style", "filled");
-    cluster.set("color", "none");
+    const cluster = g.addCluster(`Cluster_${entity.label.replace('-', '_')}`);
+    cluster.set('style', 'filled');
+    cluster.set('color', 'none');
 
-    let entry = cluster.addNode(entity.label, { shape: shape });
+    const entry = cluster.addNode(entity.label, { shape: shape });
 
-    for (let attr_name of entity.attrs ?? []) {
-      let attr = cluster.addNode(`${entity.label}.${attr_name}`, { label: attr_name });
-      cluster.addEdge(entry, attr, { arrowhead: "none" });
+    for (const attr_name of entity.attrs ?? []) {
+      const attr = cluster.addNode(`${entity.label}.${attr_name}`, { label: attr_name });
+      cluster.addEdge(entry, attr);
     }
 
     return entry;
   }
 
   add_entity(entity: Entity): graphviz.Node {
-    return this.add_cluster(entity, "box");
+    return this.add_cluster(entity, 'box');
   }
 
   add_relation(relation: Relation): graphviz.Node {
-    let g = this.inner;
-    let entry = this.add_cluster(relation, "diamond");
+    const g = this.inner;
 
-    for (let [target, arrow] of relation.arrows) {
+    let entry;
+    if (relation.attrs) {
+      entry = this.add_cluster(relation, 'diamond');
+    } else {
+      entry = g.addNode(relation.label, { shape: 'diamond' });
+    }
+
+    for (const [target, arrow] of relation.arrows) {
       g.addEdge(entry, target.label, arrow_style(arrow));
     }
 
     return entry;
   }
 
-  output(path: string) {
-    this.inner.output("png", path, console.error);
+  add_isa(isa: ISA): void {
+    const g = this.inner;
+    const { base, child } = isa;
+
+    const entry = g.addNode(`${base.label}-${child.label}-ISA`, { label: 'isa', shape: 'triangle' });
+    g.addEdge(entry, base.label, { arrowhead: 'normal' });
+    g.addEdge(entry, child.label, { arrowhead: 'none' });
+  }
+
+  output(path: string): void {
+    this.inner.render;
+    this.inner.output({ type: 'png', use: 'dot' }, path, console.error);
   }
 }
--- database/model/index.ts
+++ database/model/index.ts
@@ -1,7 +1,180 @@
-import { ERModel, Arrow, Relation, binary_relation, RelationKind } from "./ER";
-import fs from "fs";
+import fs from 'fs';
+import {
+  Relation, ERModel, binary_relation, RelationKind, Arrow,
+} from './ER';
 
-const OUTPUT_DIR = "output";
+const OUTPUT_DIR = 'output';
+
+const CUSTOMER = {
+  label: 'Customer',
+  attrs: ['name', 'address', 'phone', 'ssn'],
+};
+
+const ACCOUNT = {
+  label: 'Account',
+  attrs: ['number', 'type', 'balance'],
+};
+
+function exercise_4_1_1() {
+  const g = new ERModel('Bank');
+
+  g.add_entity(CUSTOMER);
+  g.add_entity(ACCOUNT);
+  g.add_relation(binary_relation('Own', CUSTOMER, ACCOUNT));
+
+  g.output([OUTPUT_DIR, '4_1_1.png'].join('/'));
+}
+
+function exercise_4_1_2() {
+  {
+    const g = new ERModel('Bank');
+    g.add_entity(CUSTOMER);
+    g.add_entity(ACCOUNT);
+    g.add_relation(binary_relation('Own', CUSTOMER, ACCOUNT, RelationKind.OneMany));
+
+    g.output([OUTPUT_DIR, '4_1_2_a.png'].join('/'));
+  }
+
+  {
+    const g = new ERModel('Bank');
+    g.add_entity(CUSTOMER);
+    g.add_entity(ACCOUNT);
+    g.add_relation(binary_relation('Own', CUSTOMER, ACCOUNT, RelationKind.OneOne));
+
+    g.output([OUTPUT_DIR, '4_1_2_b.png'].join('/'));
+  }
+
+  {
+    const g = new ERModel('Bank');
+    const customer = {
+      label: 'Customer',
+      attrs: ['name', 'ssn'],
+    };
+
+    const phone = {
+      label: 'Phone',
+      attrs: ['number'],
+    };
+
+    const address = {
+      label: 'Address',
+      attrs: ['street', 'city', 'state'],
+    };
+
+    g.add_entity(customer);
+    g.add_entity(phone);
+    g.add_entity(address);
+    g.add_entity(ACCOUNT);
+
+    g.add_relation(binary_relation('Own-Account', customer, ACCOUNT));
+    g.add_relation(binary_relation('Own-Phone', customer, phone));
+    g.add_relation(binary_relation('Live-in', customer, address, RelationKind.ManyOne));
+
+    g.output([OUTPUT_DIR, '4_1_2_c.png'].join('/'));
+  }
+
+  {
+    const g = new ERModel('Bank');
+    const customer = {
+      label: 'Customer',
+      attrs: ['name', 'ssn'],
+    };
+
+    const phone = {
+      label: 'Phone',
+      attrs: ['number'],
+    };
+
+    const address = {
+      label: 'Address',
+      attrs: ['street', 'city', 'state'],
+    };
+
+    g.add_entity(customer);
+    g.add_entity(phone);
+    g.add_entity(address);
+    g.add_entity(ACCOUNT);
+
+    g.add_relation(binary_relation('Own-Account', customer, ACCOUNT));
+    g.add_relation(binary_relation('Have-Phone', address, phone, RelationKind.OneMany));
+    g.add_relation(binary_relation('Live-in', customer, address, RelationKind.ManyOne));
+
+    g.output([OUTPUT_DIR, '4_1_2_d.png'].join('/'));
+  }
+}
+
+const TEAM = {
+  label: 'Team',
+  attrs: ['name'],
+};
+
+const PLAYER = {
+  label: 'Player',
+  attrs: ['name'],
+};
+
+const FAN = {
+  label: 'Fan',
+  attrs: ['name'],
+};
+
+const COLOR = {
+  label: 'Color',
+  attrs: ['name'],
+};
+
+function exercise_4_1_3() {
+
+  const g = new ERModel('Sport');
+
+  g.add_entity(TEAM);
+  g.add_entity(PLAYER);
+  g.add_entity(FAN);
+  g.add_entity(COLOR);
+
+  g.add_relation(binary_relation('Team-Players', TEAM, PLAYER, RelationKind.OneMany));
+  g.add_relation(binary_relation('Team-Captain', TEAM, PLAYER, RelationKind.OneOne));
+  g.add_relation(binary_relation('Uniform-Colors', TEAM, COLOR));
+  g.add_relation(binary_relation('Fav-Team', FAN, TEAM, RelationKind.ManyOne));
+  g.add_relation(binary_relation('Fav-Player', FAN, PLAYER, RelationKind.ManyOne));
+  g.add_relation(binary_relation('Fav-Color', FAN, COLOR, RelationKind.ManyOne));
+
+  g.output([OUTPUT_DIR, '4_1_3.png'].join('/'));
+}
+
+function exercise_4_1_4() {
+  {
+    const led_by: Relation = {
+      label: "Led-by",
+      arrows: [
+        [PLAYER, Arrow.Many],
+        [PLAYER, Arrow.Many],
+        [TEAM, Arrow.Many],
+      ]
+    };
+
+    const g = new ERModel('Sport');
+
+    g.add_entity(TEAM);
+    g.add_entity(PLAYER);
+    g.add_entity(FAN);
+    g.add_entity(COLOR);
+
+    g.add_relation(binary_relation('Team-Players', TEAM, PLAYER, RelationKind.OneMany));
+    g.add_relation(binary_relation('Team-Captain', TEAM, PLAYER, RelationKind.OneOne));
+    g.add_relation(binary_relation('Uniform-Colors', TEAM, COLOR));
+    g.add_relation(binary_relation('Fav-Team', FAN, TEAM, RelationKind.ManyOne));
+    g.add_relation(binary_relation('Fav-Player', FAN, PLAYER, RelationKind.ManyOne));
+    g.add_relation(binary_relation('Fav-Color', FAN, COLOR, RelationKind.ManyOne));
+    g.add_relation(led_by);
+
+    g.output([OUTPUT_DIR, '4_1_4_a.png'].join('/'));
+  }
+
+  {
+
+  }
+}
 
 function main() {
   if (!fs.existsSync(OUTPUT_DIR)) {
@@ -10,104 +183,8 @@ function main() {
 
   exercise_4_1_1();
   exercise_4_1_2();
-}
-
-const CUSTOMER = {
-  label: "Customer",
-  attrs: ["name", "address", "phone", "ssn"],
-};
-
-const ACCOUNT = {
-  label: "Account",
-  attrs: ["number", "type", "balance"],
-};
-
-function exercise_4_1_1() {
-  let g = new ERModel("Bank");
-
-  g.add_entity(CUSTOMER);
-  g.add_entity(ACCOUNT);
-  g.add_relation(binary_relation("Own", CUSTOMER, ACCOUNT));
-
-  g.output([OUTPUT_DIR, "4_1_1.png"].join("/"));
-}
-
-function exercise_4_1_2() {
-  {
-    let g = new ERModel("Bank");
-    g.add_entity(CUSTOMER);
-    g.add_entity(ACCOUNT);
-    g.add_relation(binary_relation("Own", CUSTOMER, ACCOUNT, RelationKind.OneMany));
-
-    g.output([OUTPUT_DIR, "4_1_2_a.png"].join("/"));
-  }
-
-  {
-    let g = new ERModel("Bank");
-    g.add_entity(CUSTOMER);
-    g.add_entity(ACCOUNT);
-    g.add_relation(binary_relation("Own", CUSTOMER, ACCOUNT, RelationKind.OneOne));
-
-    g.output([OUTPUT_DIR, "4_1_2_b.png"].join("/"));
-  }
-
-  {
-    let g = new ERModel("Bank");
-    let customer = {
-      label: "Customer",
-      attrs: ["name", "ssn"],
-    };
-
-    let phone = {
-      label: "Phone",
-      attrs: ["number"],
-    };
-
-    let address = {
-      label: "Address",
-      attrs: ["street", "city", "state"],
-    };
-
-    g.add_entity(customer);
-    g.add_entity(phone);
-    g.add_entity(address);
-    g.add_entity(ACCOUNT);
-
-    g.add_relation(binary_relation("Own-Account", customer, ACCOUNT));
-    g.add_relation(binary_relation("Own-Phone", customer, phone));
-    g.add_relation(binary_relation("Live-in", customer, address, RelationKind.ManyOne));
-
-    g.output([OUTPUT_DIR, "4_1_2_c.png"].join("/"));
-  }
-
-  {
-    let g = new ERModel("Bank");
-    let customer = {
-      label: "Customer",
-      attrs: ["name", "ssn"],
-    };
-
-    let phone = {
-      label: "Phone",
-      attrs: ["number"],
-    };
-
-    let address = {
-      label: "Address",
-      attrs: ["street", "city", "state"],
-    };
-
-    g.add_entity(customer);
-    g.add_entity(phone);
-    g.add_entity(address);
-    g.add_entity(ACCOUNT);
-
-    g.add_relation(binary_relation("Own-Account", customer, ACCOUNT));
-    g.add_relation(binary_relation("Have-Phone", address, phone, RelationKind.OneMany));
-    g.add_relation(binary_relation("Live-in", customer, address, RelationKind.ManyOne));
-
-    g.output([OUTPUT_DIR, "4_1_2_d.png"].join("/"));
-  }
+  exercise_4_1_3();
+  exercise_4_1_4();
 }
 
 main();
