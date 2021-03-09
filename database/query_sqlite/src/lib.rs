1031d25b3a0425c0bbca22b057fa085e0b89215d
ivfranco <ivfranco33@outlook.com>
2021-03-09 18:58:57 +0800

    p

--- database/query_sqlite/src/lib.rs
+++ database/query_sqlite/src/lib.rs
@@ -1,7 +1,16 @@
-use rusqlite::types::{FromSql, FromSqlError, FromSqlResult};
-use std::fmt::Debug;
+use rusqlite::{
+    params,
+    types::{FromSql, FromSqlError, FromSqlResult, ToSqlOutput, Value as SQLValue, ValueRef},
+    Connection, Result, Row, ToSql,
+};
+use std::{
+    error::Error,
+    fmt::{Debug, Display},
+    str::{from_utf8, FromStr},
+};
 
 pub enum Value {
+    String(String),
     Integer(i64),
     Float(f64),
     Null,
@@ -10,6 +19,7 @@ pub enum Value {
 impl Debug for Value {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
+            Value::String(s) => Display::fmt(s, f),
             Value::Integer(d) => Debug::fmt(d, f),
             Value::Float(r) => Debug::fmt(r, f),
             Value::Null => write!(f, "Null"),
@@ -17,13 +27,82 @@ impl Debug for Value {
     }
 }
 
+impl FromStr for Value {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let value = if let Ok(i) = s.parse::<i64>() {
+            Value::Integer(i)
+        } else if let Ok(f) = s.parse::<f64>() {
+            Value::Float(f)
+        } else {
+            Value::String(s.to_string())
+        };
+
+        Ok(value)
+    }
+}
+
 impl FromSql for Value {
     fn column_result(value: rusqlite::types::ValueRef<'_>) -> FromSqlResult<Self> {
         match value {
-            rusqlite::types::ValueRef::Null => Ok(Value::Null),
-            rusqlite::types::ValueRef::Integer(i) => Ok(Value::Integer(i)),
-            rusqlite::types::ValueRef::Real(r) => Ok(Value::Float(r)),
+            ValueRef::Null => Ok(Value::Null),
+            ValueRef::Integer(i) => Ok(Value::Integer(i)),
+            ValueRef::Real(r) => Ok(Value::Float(r)),
+            ValueRef::Text(s) => from_utf8(s)
+                .map(|s| Value::String(s.to_string()))
+                .map_err(|_| FromSqlError::InvalidType),
             _ => Err(FromSqlError::InvalidType),
         }
     }
+}
+
+impl ToSql for Value {
+    fn to_sql(&self) -> Result<rusqlite::types::ToSqlOutput<'_>> {
+        let value = match self {
+            Value::String(s) => ToSqlOutput::Borrowed(ValueRef::Text(s.as_bytes())),
+            Value::Integer(i) => ToSqlOutput::Owned(SQLValue::Integer(*i)),
+            Value::Float(f) => ToSqlOutput::Owned(SQLValue::Real(*f)),
+            Value::Null => ToSqlOutput::Owned(SQLValue::Null),
+        };
+
+        Ok(value)
+    }
+}
+
+fn collect(row: &Row) -> Result<Vec<Value>> {
+    let mut vec = vec![];
+    for i in 0..row.column_count() {
+        vec.push(row.get(i)?);
+    }
+    Ok(vec)
+}
+
+pub fn exec<P>(conn: &Connection, stmt: &str, params: P) -> Result<Vec<Vec<Value>>>
+where
+    P: IntoIterator,
+    P::Item: ToSql,
+{
+    let mut stmt = conn.prepare(stmt)?;
+    let rows = stmt.query_map(params, collect)?;
+    rows.collect()
+}
+
+pub fn exec_and_print(conn: &Connection, stmt: &str) -> Result<()> {
+    println!("{}", stmt);
+    for row in exec(conn, stmt, params![])? {
+        println!("{:?}", row);
+    }
+    Ok(())
+}
+
+pub fn insert_into(conn: &Connection, csv: &str, stmt: &str) -> Result<(), Box<dyn Error>> {
+    let reader = csv::ReaderBuilder::new().delimiter(b' ').from_path(csv)?;
+    for record in reader.into_records() {
+        let record = record?;
+        let values = record.iter().map(|s| s.parse::<Value>().unwrap());
+        conn.execute(stmt, values)?;
+    }
+
+    Ok(())
 }
--- database/query_sqlite/src/main.rs
+++ database/query_sqlite/src/main.rs
@@ -1,25 +1,12 @@
-use query_sqlite::Value;
-use rusqlite::{params, Connection, Result, Row};
+use std::error::Error;
 
-fn main() -> Result<()> {
-    exercise_5_2_1()
-}
+use query_sqlite::{exec, exec_and_print, insert_into, Value};
+use rusqlite::{params, Connection, Result};
 
-fn collect(row: &Row) -> Result<Vec<Value>> {
-    let mut vec = vec![];
-    for i in 0..row.column_count() {
-        vec.push(row.get(i)?);
-    }
-    Ok(vec)
-}
+fn main() -> Result<(), Box<dyn Error>> {
+    exercise_5_2_1()?;
+    exercise_8_3_1()?;
 
-fn exec(conn: &Connection, stmt: &str) -> Result<()> {
-    println!("{}", stmt);
-    let mut stmt = conn.prepare(stmt)?;
-    let rows = stmt.query_map(params!(), collect)?;
-    for row in rows {
-        println!("{:?}", row?);
-    }
     Ok(())
 }
 
@@ -51,7 +38,7 @@ fn exercise_5_2_1() -> Result<()> {
         conn.execute("INSERT INTO S (B, C) VALUES (?1, ?2)", &[b, c])?;
     }
 
-    let exec = |stmt: &str| exec(&conn, stmt);
+    let exec = |stmt: &str| exec_and_print(&conn, stmt);
 
     exec("SELECT A + B, A * A, B * B FROM R;")?;
     exec("SELECT B + 1, C - 1 FROM S;")?;
@@ -75,6 +62,266 @@ SELECT A, S.B, C FROM (S LEFT OUTER JOIN R ON R.B = S.B) WHERE A IS NULL;",
 UNION ALL
 SELECT A, R.B, S.B, C FROM (S LEFT OUTER JOIN R ON R.B < S.B) WHERE A IS NULL;",
     )?;
+
+    Ok(())
+}
+
+fn pc_database() -> Result<Connection, Box<dyn Error>> {
+    let conn = Connection::open_in_memory()?;
+    conn.execute_batch(
+        "BEGIN;
+        CREATE TABLE Product (
+            maker   STRING, 
+            model   INT,
+            type    STRING
+        );
+        CREATE TABLE PC (
+            model   INT PRIMERY KEY,
+            speed   FLOAT,
+            ram     INT,
+            hd      INT,
+            price   INT
+        );
+        CREATE TABLE Laptop (
+            model   INT PRIMERY KEY,
+            speed   FLOAT,
+            ram     INT,
+            hd      INT,
+            screen  FLOAT,
+            price   INT
+        );
+        CREATE TABLE Printer (
+            model   INT PRIMERY KEY,
+            color   STRING,
+            type    STRING,
+            price   INT
+        );
+        COMMIT;
+        ",
+    )?;
+
+    insert_into(
+        &conn,
+        "../query/relations/Product.csv",
+        "INSERT INTO Product VALUES (?1, ?2, ?3)",
+    )?;
+    insert_into(
+        &conn,
+        "../query/relations/PC.csv",
+        "INSERT INTO PC VALUES (?1, ?2, ?3, ?4, ?5)",
+    )?;
+    insert_into(
+        &conn,
+        "../query/relations/Laptop.csv",
+        "INSERT INTO Laptop VALUES (?1, ?2, ?3, ?4, ?5, ?6)",
+    )?;
+    insert_into(
+        &conn,
+        "../query/relations/Printer.csv",
+        "INSERT INTO Printer VALUES (?1, ?2, ?3, ?4)",
+    )?;
+
+    Ok(conn)
+}
+
+fn exercise_8_3_1() -> Result<(), Box<dyn Error>> {
+    let mut conn = pc_database()?;
+
+    fn closest_price(conn: &Connection, price: i64) -> Result<(String, i64, f64)> {
+        let mut stmt = conn.prepare(
+            "SELECT maker, PC.model, speed, price
+            FROM Product, PC
+            WHERE Product.model = PC.model
+            ",
+        )?;
+
+        let rows = stmt.query_map(params![], |row| {
+            let maker: String = row.get(0)?;
+            let model: i64 = row.get(1)?;
+            let speed: f64 = row.get(2)?;
+            let price: i64 = row.get(3)?;
+
+            Ok((maker, model, speed, price))
+        })?;
+
+        let (maker, model, speed, ..) = rows
+            .filter_map(|r| r.ok())
+            .min_by_key(|(.., p)| (price - p).abs())
+            .unwrap();
+
+        Ok((maker, model, speed))
+    }
+
+    assert_eq!(closest_price(&conn, 1000)?.1, 1002);
+
+    #[derive(Debug)]
+    struct Laptop {
+        model: i64,
+        speed: f64,
+        ram: i64,
+        hd: i64,
+        screen: f64,
+        price: i64,
+    }
+
+    fn minimum_requirement(
+        conn: &Connection,
+        speed: f64,
+        ram: i64,
+        hd: i64,
+        screen: f64,
+    ) -> Result<Vec<(String, Laptop)>> {
+        let mut stmt = conn.prepare(
+            "SELECT maker, Laptop.model, speed, ram, hd, screen, price
+            FROM Product, Laptop
+            WHERE 
+                Product.model = Laptop.model AND 
+                speed >= ?1 AND 
+                ram >= ?2 AND 
+                hd >= ?3 AND 
+                screen >= ?4
+            ",
+        )?;
+
+        let rows = stmt.query_map(params![speed, ram, hd, screen], |row| {
+            let maker: String = row.get("maker")?;
+            let laptop = Laptop {
+                model: row.get("model")?,
+                speed: row.get("speed")?,
+                ram: row.get("ram")?,
+                hd: row.get("hd")?,
+                screen: row.get("screen")?,
+                price: row.get("price")?,
+            };
+
+            Ok((maker, laptop))
+        })?;
+
+        rows.into_iter().collect()
+    }
+
+    assert_eq!(
+        {
+            let (_, laptop) = &minimum_requirement(&conn, 2.0, 2048, 240, 20.0)?[0];
+            laptop.model
+        },
+        2001
+    );
+
+    fn all_products(conn: &Connection, maker: &str) -> Result<Vec<Vec<Value>>> {
+        let mut products = vec![];
+        products.extend(exec(
+            conn,
+            "SELECT *
+            FROM (Product NATURAL JOIN PC)
+            WHERE maker = ?1
+            ",
+            &[maker],
+        )?);
+        products.extend(exec(
+            conn,
+            "SELECT *
+            FROM (Product NATURAL JOIN Laptop)
+            WHERE maker = ?1
+            ",
+            &[maker],
+        )?);
+        products.extend(exec(
+            conn,
+            "SELECT *
+            FROM (Product NATURAL JOIN Printer)
+            WHERE maker = ?1
+            ",
+            &[maker],
+        )?);
+
+        Ok(products)
+    }
+
+    assert_eq!(all_products(&conn, "B")?.len(), 4);
+
+    fn budget(conn: &Connection, budget: i64, speed: i64) -> Result<Option<(i64, i64)>> {
+        let mut stmt = conn.prepare(
+            "SELECT PC.model, Printer.model
+            FROM PC, Printer
+            WHERE 
+                PC.price + Printer.price <= ?1 AND 
+                PC.speed >= ?2
+            ORDER BY Printer.color <> 'true', PC.price + Printer.price
+            ",
+        )?;
+
+        let mut rows = stmt.query_map(params![budget, speed], |row| {
+            let pc_model: i64 = row.get(0)?;
+            let printer_model: i64 = row.get(1)?;
+
+            Ok((pc_model, printer_model))
+        })?;
+
+        rows.next().transpose()
+    }
+
+    assert_eq!(budget(&conn, 100000, 0)?, Some((1003, 3001)));
+
+    #[derive(Clone, Default)]
+    struct PC {
+        model: i64,
+        speed: f64,
+        ram: i64,
+        hd: i64,
+        price: i64,
+    }
+
+    fn new_pc(conn: &mut Connection, maker: &str, pc: &PC) -> Result<bool> {
+        let tran = conn.transaction()?;
+        let row = tran.query_row(
+            "SELECT *
+            FROM PC
+            WHERE model = ?1
+            ",
+            &[pc.model],
+            |_| Ok(()),
+        );
+
+        if row.is_ok() {
+            eprintln!("Error: existing model");
+            return Ok(false);
+        }
+
+        tran.execute(
+            "INSERT INTO Product
+            VALUES (?1, ?2, ?3)
+            ",
+            params![maker, pc.model, "pc"],
+        )?;
+
+        tran.execute(
+            "INSERT INTO PC
+            VALUES (?1, ?2, ?3, ?4, ?5)
+            ",
+            params![pc.model, pc.speed, pc.ram, pc.hd, pc.price],
+        )?;
+
+        Ok(true)
+    }
+
+    assert!(!new_pc(
+        &mut conn,
+        "A",
+        &PC {
+            model: 1003,
+            ..PC::default()
+        }
+    )?,);
+
+    assert!(new_pc(
+        &mut conn,
+        "A",
+        &PC {
+            model: 8086,
+            ..PC::default()
+        }
+    )?,);
 
     Ok(())
 }
