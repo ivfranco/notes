e33833bbc1f50fd05d86c956e50b8ef1c8bd32c0
ivfranco <ivfranco33@outlook.com>
2021-03-10 19:10:20 +0800

    p

--- database/query_sqlite/src/main.rs
+++ database/query_sqlite/src/main.rs
@@ -1,16 +1,19 @@
-use std::error::Error;
-
 use query_sqlite::{exec, exec_and_print, insert_into, Value};
-use rusqlite::{params, Connection, Result};
+use rusqlite::{params, Connection, Result, Transaction};
+use std::error::Error;
+use std::io;
 
 fn main() -> Result<(), Box<dyn Error>> {
     exercise_5_2_1()?;
     exercise_8_3_1()?;
+    exercise_8_3_2()?;
 
     Ok(())
 }
 
 fn exercise_5_2_1() -> Result<()> {
+    println!("\nexercise 5.2.1");
+
     const R: [(i32, i32); 5] = [(0, 1), (2, 3), (0, 1), (2, 4), (3, 4)];
     const S: [(i32, i32); 6] = [(0, 1), (2, 4), (2, 5), (3, 4), (0, 2), (3, 4)];
 
@@ -125,6 +128,8 @@ fn pc_database() -> Result<Connection, Box<dyn Error>> {
 }
 
 fn exercise_8_3_1() -> Result<(), Box<dyn Error>> {
+    println!("\nexercise 8.3.1");
+
     let mut conn = pc_database()?;
 
     fn closest_price(conn: &Connection, price: i64) -> Result<(String, i64, f64)> {
@@ -322,6 +327,290 @@ fn exercise_8_3_1() -> Result<(), Box<dyn Error>> {
             ..PC::default()
         }
     )?,);
+
+    Ok(())
+}
+
+fn ship_database() -> Result<Connection, Box<dyn Error>> {
+    let conn = Connection::open_in_memory()?;
+    conn.execute_batch(
+        "BEGIN;
+        CREATE TABLE Classes (
+            class   STRING PRIMARY KEY,
+            type    STRING,
+            country STRING,
+            numGuns INT,
+            bore    INT,
+            displacement    INT
+        );
+        CREATE TABLE Ships (
+            name    STRING PRIMARY KEY,
+            class   STRING,
+            launched    INT
+        );
+        CREATE TABLE Battles (
+            name    STRING PRIMARY KEY,
+            date    STRING
+        );
+        CREATE TABLE Outcomes (
+            ship    STRING,
+            battle  STRING,
+            result  STRING,
+
+            PRIMARY KEY (ship, battle)
+        );
+        COMMIT;
+        ",
+    )?;
+
+    insert_into(
+        &conn,
+        "../query/relations/Classes.csv",
+        "INSERT INTO Classes VALUES (?1, ?2, ?3, ?4, ?5, ?6)",
+    )?;
+    insert_into(
+        &conn,
+        "../query/relations/Ships.csv",
+        "INSERT INTO Ships VALUES (?1, ?2, ?3)",
+    )?;
+    insert_into(
+        &conn,
+        "../query/relations/Battles.csv",
+        "INSERT INTO Battles VALUES (?1, ?2)",
+    )?;
+    insert_into(
+        &conn,
+        "../query/relations/Outcomes.csv",
+        "INSERT INTO Outcomes VALUES (?1, ?2, ?3)",
+    )?;
+
+    Ok(conn)
+}
+
+fn max_firepower(conn: &Connection) -> Result<String> {
+    conn.query_row(
+        "SELECT class
+            FROM Classes
+            ORDER BY numGuns * bore * bore * bore DESC
+            ",
+        params![],
+        |row| row.get(0),
+    )
+}
+
+fn casualty(conn: &Connection, battle: &str) -> Result<(Option<String>, Option<String>)> {
+    let sunk: Option<String> = conn
+        .query_row(
+            "SELECT country
+            FROM Classes, Ships, Outcomes
+            WHERE
+                Classes.class = Ships.class AND
+                Ships.name = Outcomes.ship AND
+                Outcomes.battle = ?1
+            GROUP BY country
+            ORDER BY SUM(result = 'sunk') DESC
+            ",
+            &[battle],
+            |row| row.get(0),
+        )
+        .or_else(|e| match e {
+            rusqlite::Error::QueryReturnedNoRows => Ok(None),
+            _ => Err(e),
+        })?;
+
+    let damaged: Option<String> = conn
+        .query_row(
+            "SELECT country
+            FROM Classes, Ships, Outcomes
+            WHERE
+                Classes.class = Ships.class AND
+                Ships.name = Outcomes.ship AND
+                Outcomes.battle = ?1
+            GROUP BY country
+            ORDER BY SUM(result = 'damaged') DESC
+            ",
+            &[battle],
+            |row| row.get(0),
+        )
+        .or_else(|e| match e {
+            rusqlite::Error::QueryReturnedNoRows => Ok(None),
+            _ => Err(e),
+        })?;
+
+    Ok((sunk, damaged))
+}
+
+#[derive(Default)]
+struct Class {
+    class: String,
+    ty: String,
+    country: String,
+    num_guns: i64,
+    bore: i64,
+    displacement: i64,
+}
+
+#[derive(Default)]
+struct Ship {
+    name: String,
+    launched: i64,
+}
+
+fn insert_ships(conn: &mut Connection, class: &Class, ships: &[Ship]) -> Result<()> {
+    let tran = conn.transaction()?;
+    let inserted = tran.execute(
+        "INSERT INTO Classes
+            VALUES (?1, ?2, ?3, ?4, ?5, ?6)
+            ",
+        params![
+            &class.class,
+            &class.ty,
+            &class.country,
+            class.num_guns,
+            class.bore,
+            class.displacement
+        ],
+    )?;
+    assert_eq!(inserted, 1);
+
+    let inserted = ships
+        .iter()
+        .map(|ship| {
+            tran.execute(
+                "INSERT INTO Ships
+                VALUES (?1, ?2, ?3)
+                ",
+                params![&ship.name, &class.class, &ship.launched],
+            )
+        })
+        .sum::<Result<usize>>()?;
+
+    assert_eq!(inserted, ships.len());
+    Ok(())
+}
+
+fn fix_date(conn: &mut Connection) -> Result<(), Box<dyn Error>> {
+    let tran = conn.transaction()?;
+    let mut stmt = tran.prepare(
+        "SELECT 
+                Ships.name AS ship,
+                Ships.launched,
+                Battles.name AS battle,
+                CAST (('19' || SUBSTR(Battles.date, -2, 2)) AS INT) AS battled
+            FROM Ships, Battles, Outcomes
+            WHERE
+                Ships.name = Outcomes.ship AND
+                Battles.name = Outcomes.battle AND
+                launched > battled
+            ",
+    )?;
+
+    let rows = stmt.query_map(params![], |row| {
+        let ship: String = row.get("ship")?;
+        let launched: i64 = row.get("launched")?;
+        let battle: String = row.get("battle")?;
+        let battled: i64 = row.get("battled")?;
+
+        Ok((ship, launched, battle, battled))
+    })?;
+
+    let mut errors = rows.collect::<Result<Vec<_>>>()?;
+
+    while let Some((ship, launched, battle, battled)) = errors.pop() {
+        loop {
+            println!(
+                "{} launched in {} after participating {} in {}",
+                ship, launched, battle, battled
+            );
+            println!("which date should be updated? (type 'ship' or 'battle'):");
+            let input = stdin_read_line()?;
+            match input.as_str() {
+                "ship" => {
+                    fix_ship_date(&tran, &ship, battled)?;
+                    break;
+                }
+                "battle" => {
+                    fix_battle_date(&tran, &battle, launched)?;
+                    errors.retain(|(_, l, b, _)| !(b != &battle && *l <= launched));
+                    break;
+                }
+                _ => {
+                    println!("type again.");
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn stdin_read_line() -> io::Result<String> {
+    let stdin = io::stdin();
+    let mut buf = String::new();
+    stdin.read_line(&mut buf)?;
+    buf = buf.trim().to_string();
+    Ok(buf)
+}
+
+fn fix_ship_date(tran: &Transaction, ship: &str, date: i64) -> Result<()> {
+    tran.execute(
+        "UPDATE Ships
+        SET launched = ?1
+        WHERE name = ?2
+        ",
+        params![date, ship],
+    )?;
+    Ok(())
+}
+
+fn fix_battle_date(tran: &Transaction, battle: &str, date: i64) -> Result<()> {
+    let date = date.to_string();
+    tran.execute(
+        "UPDATE Battles
+        SET date = SUBSTR(date, 0, LENGTH(date) - 2) || SUBSTR(?1, -2, 2)
+        WHERE name = ?2
+        ",
+        params![date, battle],
+    )?;
+    Ok(())
+}
+
+fn exercise_8_3_2() -> Result<(), Box<dyn Error>> {
+    println!("\nexercise 8.3.2");
+
+    let mut conn = ship_database()?;
+
+    assert_eq!(max_firepower(&conn)?, "Yamato");
+
+    assert_eq!(
+        casualty(&conn, "Surigao Strait")?,
+        (Some("USA".to_owned()), Some("USA".to_owned()))
+    );
+
+    assert!(insert_ships(
+        &mut conn,
+        &Class {
+            class: "Moltke".to_string(),
+            ty: "bc".to_string(),
+            country: "Germany".to_string(),
+            num_guns: 10,
+            bore: 11,
+            displacement: 22979
+        },
+        &[
+            Ship {
+                name: "Moltke".to_string(),
+                launched: 1910,
+            },
+            Ship {
+                name: "Goeben".to_string(),
+                launched: 1911,
+            }
+        ]
+    )
+    .is_ok());
+
+    assert!(fix_date(&mut conn).is_ok());
 
     Ok(())
 }
